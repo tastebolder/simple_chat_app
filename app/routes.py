69ec000717c783f71c4b312403b60e0978e11ef2
tastebolder <tastebolder@gmail.com>
2022-10-21 23:54:01 +0300

    first commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello flask'
+    """Index URL"""
+    return render_template('index.html', title='Index Page')
+
+
+@app.route('/about-me')
+def about_me():
+    """About Me URL"""
+    return render_template('about_me.html', title='About Me Page')
---
+++ app/templates/README.md
@@ -0,0 +1,11 @@
+# Hello Flask
+
+This is a sample project used to show how to work with Templates
+
+## Technologies Used
+
+- Flask
+- Python
+- Jinja Templates
+
+The concept of template inheritance has been used.
